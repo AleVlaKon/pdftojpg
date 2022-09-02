d7c9544bddf8a26f35ce6c3d170428ff7dc8400e
Наташенька <kontikov@mail.ru>
2022-09-02 23:33:22 +0300

    Version Loaders

--- jinja.py
+++ jinja.py
@@ -1,28 +1,27 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader, FunctionLoader
 
 persons = [
     {"name": "Алексей", "old": 18, "weight": 78.5},
     {"name": "Николай", "old": 28, "weight": 82.3},
     {"name": "Иван", "old": 33, "weight": 94.0}
 ]
+#
+# file_loader = FileSystemLoader('templates')
+# env = Environment(loader=file_loader)
+#
+# tm = env.get_template('main.html')
+# msg = tm.render(users = persons)
 
-html = '''
-{% macro list_users(list_of_user) -%}
-<ul>
-{% for u in users -%}
-    <li>{{u.name}} {{caller(u)}}
-{%- endfor %}
-</ul>
-{%- endmacro %}
- 
-{% call(user) list_users(users) %}
-    <ul>
-    <li>age: {{user.old}}
-    <li>weight: {{user.weight}}
-    </ul>
-{% endcall -%}
-'''
+def loadTpl(path):
+    if path == "index":
+        return '''Имя {{u.name}}, возраст {{u.old}}'''
+    else:
+        return '''Данные: {{u}}'''
 
-tm = Template(html)
-msg = tm.render(users = persons)
+file_loader = FunctionLoader(loadTpl)
+env = Environment(loader=file_loader)
+
+tm = env.get_template('index')
+msg = tm.render(u = persons[0])
+
 print(msg)
